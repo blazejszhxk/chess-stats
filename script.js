ba1ce9229c21d8817ea0995f268c4488306b44ac
blazej <88905059+blazejszhxk@users.noreply.github.com>
2025-03-06 00:38:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -20,58 +20,26 @@ const usersMap = {
 
 const dataDir = path.join(process.cwd(), 'dane');
 if (!fs.existsSync(dataDir)) {
-    fs.mkdirSync(dataDir, { recursive: true });
-}
-
-async function delay(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-function deleteExistingFiles() {
-    try {
-        const files = fs.readdirSync(dataDir);
-        if (files.length === 0) {
-            console.log('Brak plików do usunięcia.');
-        }
-
-        for (const file of files) {
-            const filePath = path.join(dataDir, file);
-            if (fs.existsSync(filePath)) {
-                try {
-                    fs.unlinkSync(filePath);
-                    console.log(`Usunięto plik: ${filePath}`);
-                } catch (err) {
-                    console.error(`Błąd przy usuwaniu pliku: ${filePath}`, err);
-                }
-            }
-        }
-    } catch (err) {
-        console.error('Błąd przy odczytywaniu plików z katalogu', err);
-    }
+    fs.mkdirSync(dataDir);
 }
 
 async function fetchChessStats(username) {
     const url = `https://www.chess.com/callback/member/stats/${username}`;
     try {
         const response = await fetch(url);
-        if (!response.ok) throw new Error(`HTTP error ${response.status}`);
+        if (!response.ok) throw new Error(HTTP error ${response.status});
         const jsonData = await response.json();
-
-        const filePath = path.join(dataDir, `${username}.json`);
-        
+        const filePath = path.join(dataDir, ${username}.json);
         fs.writeFileSync(filePath, JSON.stringify(jsonData, null, 2));
-        console.log(`Dane zapisane w pliku ${filePath}`);
+        console.log(Dane zapisane w pliku ${filePath});
     } catch (error) {
-        console.error(`Błąd pobierania danych dla ${username}: ${error.message}`);
+        console.error(Błąd pobierania danych dla ${username}: ${error.message});
     }
 }
 
 async function fetchAllStats() {
-    deleteExistingFiles();
-    
     for (const userData of Object.values(usersMap)) {
         await fetchChessStats(userData.username);
-        await delay(1000);
     }
 }
 
