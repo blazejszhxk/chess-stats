db19e063d2e3f75824b5868c1fbb0942dcba8c1d
blazej <88905059+blazejszhxk@users.noreply.github.com>
2025-03-07 00:04:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ async function fetchChessStats(username) {
     const url = `https://www.chess.com/callback/member/stats/${username}`;
     try {
         const response = await fetch(url);
-        if (!response.ok) throw new Error(HTTP error ${response.status});
+        if (!response.ok) throw new Error(HTTP error ${response.status}));
         const jsonData = await response.json();
         const filePath = path.join(dataDir, ${username}.json);
         fs.writeFileSync(filePath, JSON.stringify(jsonData, null, 2));
